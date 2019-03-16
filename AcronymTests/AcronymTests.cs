8a57df0f5052bc7c838a9331d4d4593d9074d44a
Jozef Pastucha <xpastuch@fi.muni.cz>
2019-03-17 00:29:17 +0100

    Initial Acronym and AcronymTests projects

---
+++ AcronymTests/AcronymTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AcronymTests
+{
+    public class AcronymTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
